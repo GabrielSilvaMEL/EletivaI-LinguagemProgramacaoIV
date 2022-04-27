af006512493e66d7d96e53eb1bd03e166237fe85
GabrielSilvaMEL <99516302+GabrielSilvaMEL@users.noreply.github.com>
2022-04-27 20:27:45 -0300

    Update matricial.php

--- Aulas2/Aula 2704/matricial.php
+++ Aulas2/Aula 2704/matricial.php
@@ -4,7 +4,16 @@ class Matricial extends Impressora {
     private $numAgulhas;
     private $imprimeVias;
 
-
+    public function __construct($marca, $modelo, $numAgulhas, $colorida, $imprimeVias, $peso)
+    {
+        $this->setMarca($marca);
+        $this->setModelo($modelo);
+        $this->setNumAgulhas($numAgulhas);
+        $this->setColorida($colorida);
+        $this->setImprimeVias($imprimeVias);
+        $this->setPeso($peso);
+    }
+    
     public function getNumAgulhas(){
         return $this->numagulhas;
     }
