ba0f5fcf0eb6edebe0dbc156a02cd48d3ab9ba90
GabrielSilvaMEL <99516302+GabrielSilvaMEL@users.noreply.github.com>
2022-04-29 21:26:30 -0300

    Create Ponto.php

---
+++ POO/Ponto.php
@@ -0,0 +1,39 @@
+<?php
+
+    Class Ponto{
+        private $x;
+        private $y;
+        private static $contador;
+
+
+        //construtor
+        public function __construct($x, $y)
+        {
+            $this->setX($x);
+            $this->setY($y);
+            
+        }
+        //Usa-se $this para acessar propriedades e métodos que fazem escopo de um objeto. 
+        //Usa-se self para acessar propriedades e métodos que fazem parte do escopo da classe.
+        public static function setContador($contador){
+            self::$contador = $contador;
+        }
+        public static function getContador(){
+            return self:: $contador;
+        }
+        //get e set
+        public function getX(){
+            return $this->x;
+        }
+        public function setX($x){
+            $this->x = $x;
+        }
+
+        public function getY(){
+            return $this->y;
+        }
+        public function setY($y){
+            $this->y = $y;
+        }
+    }
+?>
