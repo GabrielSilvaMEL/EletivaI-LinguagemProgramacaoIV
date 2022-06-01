9ac3fd8235b47f1f8c47032634c8633355355fb0
GabrielSilvaMEL <99516302+GabrielSilvaMEL@users.noreply.github.com>
2022-06-01 20:13:04 -0300

    Update ProdutosDAO.php

--- projetophp/src/Model/DAO/ProdutosDAO.php
+++ projetophp/src/Model/DAO/ProdutosDAO.php
@@ -5,18 +5,29 @@ namespace Aluno\projetophp\Model\DAO;
 use Aluno\projetophp\Model\Entity\Produtos;
 
 class ProdutosDAO{
-    public function inserir(Produtos $p){
+    public function inserir(Produtos $pr){
         try{
-            $sql = "INSERT INTO `produtos`(`nome`, `descricao`, `valor`) VALUES (:nome, :descricao, :valor)";
+            $sql = "INSERT INTO `produtos`( `nome`, `descricao`, `valor`) VALUES (:nome, :descricao, :valor)";
+            $p = Conexao::conectar()->prepare($sql);
+            $p->bindValue(":nome",$pr->getNome());
+            $p->bindValue(":descricao",$pr->getDescricao());
+            $p->bindValue(":valor",$pr->getValor());
+            $p->execute();
+            return $p->fetch();
         } catch(\Exception $e){
             return false;
         }
     }
 
-    public function alterar(Produtos $p){
+    public function alterar(Produtos $pr){
         try{
-            $sql = "UPDATE `produtos` SET `nome`=:nome,`descricao`=:descricao,`valor`=:valor WHERE id = :id";
-            
+            $sql = "UPDATE `produtos` SET `nome`= :nome,`descricao`= :descricao,`valor`=:valor WHERE id = :id";
+            $p = Conexao::conectar()->prepare($sql);
+            $p->bindValue(":nome",$pr->getNome());
+            $p->bindValue(":valor",$pr->getValor());
+            $p->bindValue(":valor",$pr->getValor());
+            $p->bindValue(":id",$pr->getId());
+            return $p->execute();
 
         } catch(\Exception $e){
             return false;
@@ -26,8 +37,10 @@ class ProdutosDAO{
 
     public function excluir($id){
         try{
-            $sql = "DELETE FROM `produtos` WHERE id = :id";
-
+            $sql = "DELETE FROM `clientes` WHERE id = :id";
+            $p = Conexao::conectar()->prepare($sql);
+            $p->bindValue(":id", $id);
+            return $p->execute();
         } catch(\Exception $e){
             return false;
         }
@@ -36,8 +49,8 @@ class ProdutosDAO{
 
     public function consultar(){
         try{
-            $sql = "SELECT * FROM produtos";
-            
+            $sql = "SELECT * FROM clientes";
+            return Conexao::conectar()->query($sql);
 
         } catch(\Exception $e){
             return false;
@@ -47,8 +60,10 @@ class ProdutosDAO{
 
     public function consultarPorId($id){
         try{
-            $sql = "SELECT * FROM produtos WHERE id = :id";
-            
+            $sql = "SELECT * FROM clientes WHERE id = :id";
+            $p = Conexao::conectar()->prepare($sql);
+            $p->bindValue(":id", $id);
+            return $p->execute();
 
         } catch(\Exception $e){
             return false;
